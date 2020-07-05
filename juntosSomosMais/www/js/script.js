e8c3cf3847d394d4dd486ee62ac67631433c93bc
Anderson Caldeira Sobrinho <andersoncaldeirasobrinho@gmail.com>
2020-07-05 19:16:14 -0300

    finalização do front-end

--- juntosSomosMais/www/js/script.js
+++ juntosSomosMais/www/js/script.js
@@ -49,26 +49,42 @@ function trocaTela(botao) {
             left: "0",
             width: "40vw",
             height: "25vh"
-          });
-          ajuda.css("background", "#990003");
-          ajuda.css("border-radius", "1em 1em 0 0");
-          ajuda.css("box-shadow", "0px 15px 20px #000000");
+          }).css(
+            "background", "#990003"
+            ).css(
+              "border-radius", "1em 1em 0 0"
+              ).css(
+                "box-shadow", "0px 15px 20px #000000"
+                );
+
           localiza.animate({
             left:"0",
             width: "30vw",
             height: "20vh"
-          });
-          localiza.css("background", "#1F2324");
-          localiza.css("border-radius", "0");
-          localiza.css("box-shadow", "0px -13px 20px rgba(0, 0, 0, 0.27)");
+          }).css(
+            "background",
+            "#1F2324"
+          ).css(
+            "border-radius",
+            "0"
+          ).css(
+            "box-shadow",
+            "0px -13px 20px rgba(0, 0, 0, 0.27)"
+          );
           contato.animate({
             left: "0",
             width: "30vw",
             height: "20vh"
-          });
-          contato.css("background", "#1F2324");
-          contato.css("border-radius", "0");
-          contato.css("box-shadow", "0px -13px 20px rgba(0, 0, 0, 0.27)");
+          }).css(
+            "background",
+            "#1F2324"
+            ).css(
+              "border-radius",
+              "0"
+              ).css(
+                "box-shadow",
+                "0px -13px 20px rgba(0, 0, 0, 0.27)"
+          );
         }else if($(this).attr("id") == "contato"){
           cAj.animate({
             left:"100vw",
@@ -86,26 +102,44 @@ function trocaTela(botao) {
             left: "40vw",
             width: "30vw",
             height: "20vh"
-          });
-          ajuda.css("background", "#1F2324");
-          ajuda.css("border-radius", "0");
-          ajuda.css("box-shadow", "0px -13px 20px rgba(0, 0, 0, 0.27)");
+          }).css(
+            "background",
+            "#1F2324"
+            ).css(
+              "border-radius",
+              "0"
+              ).css(
+                "box-shadow",
+                "0px -13px 20px rgba(0, 0, 0, 0.27)"
+                );
           localiza.animate({
             left:"0",
             width: "30vw",
             height: "20vh"
-          });
-          localiza.css("background", "#1F2324");
-          localiza.css("border-radius", "0");
-          localiza.css("box-shadow", "0px -13px 20px rgba(0, 0, 0, 0.27)");
+          }).css(
+            "background",
+            "#1F2324"
+            ).css(
+              "border-radius",
+              "0"
+              ).css(
+                "box-shadow",
+                "0px -13px 20px rgba(0, 0, 0, 0.27)"
+                );
           contato.animate({
             left: "-30vw",
             width: "40vw",
             height: "25vh"
-          });
-          contato.css("background", "#990003");
-          contato.css("border-radius", "1em 1em 0 0");
-          contato.css("box-shadow", "0px 15px 20px #000000");
+          }).css(
+            "background",
+            "#990003"
+            ).css(
+              "border-radius",
+              "1em 1em 0 0"
+              ).css(
+                "box-shadow",
+                "0px 15px 20px #000000"
+                );
         }else if($(this).attr("id") == "localiza"){
           cAj.animate({
             left:"-100vw",
@@ -123,26 +157,43 @@ function trocaTela(botao) {
             left: "-40vw",
             width: "30vw",
             height: "20vh"
-          });
-          ajuda.css("background", "#1F2324");
-          ajuda.css("border-radius", "0");
-          ajuda.css("box-shadow", "0px -13px 20px rgba(0, 0, 0, 0.27)");
+          }).css(
+            "background",
+            "#1F2324"
+            ).css(
+              "border-radius","0"
+              ).css(
+                "box-shadow",
+                "0px -13px 20px rgba(0, 0, 0, 0.27)"
+                );
           localiza.animate({
             left: "30vw",
             width: "40vw",
             height: "25vh"
-          });
-          localiza.css("background", "#990003");
-          localiza.css("border-radius", "1em 1em 0 0");
-          localiza.css("box-shadow", "0px 15px 20px #000000");
+          }).css(
+            "background",
+            "#990003"
+            ).css(
+              "border-radius",
+              "1em 1em 0 0")
+              .css(
+                "box-shadow",
+                "0px 15px 20px #000000"
+                );
           contato.animate({
             left: "0",
             width: "30vw",
             height: "20vh"
-          });
-          contato.css("background", "#1F2324");
-          contato.css("border-radius", "0");
-          contato.css("box-shadow", "0px -13px 20px rgba(0, 0, 0, 0.27)");
+          }).css(
+            "background",
+            "#1F2324"
+            ).css(
+              "border-radius",
+              "0"
+              ).css(
+                "box-shadow",
+                "0px -13px 20px rgba(0, 0, 0, 0.27)"
+                );
         }else{
           alert("Erro de conexão!!!");
         }
